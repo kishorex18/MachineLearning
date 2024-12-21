79f968f0aaba7121380e2f8ba43207bf4aea534f
kishorex18 <kishoredharun04@gmail.com>
2024-12-21 20:58:12 +0530

    First ulpoad

---
+++ numpybasics.py
@@ -0,0 +1,9 @@
+import numpy as np
+data=np.random.rand(2,3,4)
+zeroes=np.zeros((2,2,2))
+full=np.full((2,2,2),7)
+ones=np.ones((2,2,2))
+print("data:\n",data)
+print("zeroes:\n",zeroes)
+print("full\n",full)
+print("ones\n",ones)
